317a46396d5d58f1b8e88b696ad6f552e3826124
Andrei Fidelman <andrei.fidelman@actum.cz>
2018-05-06 17:08:18 +0200

    TypeScript - Using Typings and Loading From node_modules

--- up-and-running-with-typescript/src/main.ts
+++ up-and-running-with-typescript/src/main.ts
@@ -2,6 +2,10 @@ import { Person, SocialNetwork } from './social-network';
 
 import * as _ from 'lodash';
 
+import * as Rx from "rxjs";
+
+Rx.Observable.timer(1000).subscribe((x: any) => console.log(x));
+
 class App implements SocialNetwork {
   title = 'Eggheads';
   getUsers() {
