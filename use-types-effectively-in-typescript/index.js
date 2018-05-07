09f3574738a7b2ab48be8c123573f7cd07630bb7
Andrei Fidelman <andrei.fidelman@actum.cz>
2018-05-07 22:33:17 +0200

    use-types-effectively-in-typescript: Using Type Inference in TypeScript

---
+++ use-types-effectively-in-typescript/index.js
@@ -0,0 +1,4 @@
+var x = 'hello';
+var fn = function (a, b) { return a + b; };
+var target = document.getElementById('target');
+target.onclick = function (event) { return event.button; };
---
+++ use-types-effectively-in-typescript/index.ts
@@ -0,0 +1,6 @@
+const x: string = 'hello';
+
+const fn = (a: string, b:string): string => a + b;
+
+const target: HTMLElement = document.getElementById('target');
+target.onclick = (event: MouseEvent): number => event.button;
