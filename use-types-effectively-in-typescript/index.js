b71ade3564949e1e58c80793c8008afa36c3bf99
Andrei Fidelman <andrei.fidelman@actum.cz>
2018-05-08 14:08:55 +0200

    Using Assertion to Convert Types in TypeScript

--- use-types-effectively-in-typescript/index.js
+++ use-types-effectively-in-typescript/index.js
@@ -1,5 +1,18 @@
 (function () {
-    // public (default)
-    // private – can be used only in Class
-    // protected – can be used in Extended Class as well
+    var animal = {
+        type: 'dog'
+    };
+    var human = {
+        name: 'Petr'
+    };
+    function showTypeOrName(stuff) {
+        var result = '';
+        if (stuff.name) { // old syntax
+            result = stuff.name;
+        }
+        else if (stuff.type) { // new syntax is more preferable bcz of JSX
+            result = stuff.type;
+        }
+        return result;
+    }
 })();
--- use-types-effectively-in-typescript/index.ts
+++ use-types-effectively-in-typescript/index.ts
@@ -1,5 +1,29 @@
 (function() {
-  // public (default)
-  // private – can be used only in Class
-  // protected – can be used in Extended Class as well
+  type Animal = {
+    type: string;
+  }
+
+  type Human = {
+    name: string;
+  }
+
+  const animal: Animal = {
+    type: 'dog'
+  };
+
+  const human: Human = {
+    name: 'Petr'
+  };
+
+  function showTypeOrName(stuff: Animal | Human): string {
+    let result: string = '';
+
+    if ((<Human>stuff).name) {            // old syntax
+      result = (<Human>stuff).name;
+    } else if ((stuff as Animal).type) {  // new syntax is more preferable bcz of JSX
+      result = (stuff as Animal).type;
+    }
+
+    return result;
+  }
 })();
