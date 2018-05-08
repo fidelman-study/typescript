441b415d8e2c16a43495fb5dbc5434a26258f37d
Andrei Fidelman <andrei.fidelman@actum.cz>
2018-05-08 13:05:57 +0200

    Using Interfaces to Describe Types in TypeScript

--- use-types-effectively-in-typescript/index.js
+++ use-types-effectively-in-typescript/index.js
@@ -1,2 +1,26 @@
-var unit = 'unit';
-var miles = 'MILES';
+(function () {
+    /**
+     * Interface VS Type alias
+     * 1. Type Alias union cannot be used for `implements`
+     * 2. Type Alias union cannot be used for `extends`
+     * 3. Declaration merging doesn't work with type alias
+     */
+    var superHero = {
+        secretIdentity: '1',
+        alias: '1',
+        health: 1
+    };
+    var superVillain = {
+        secretIdentity: '1',
+        alias: '1',
+        health: 1,
+        name: 'hi'
+    };
+    var spawn = {
+        name: 'Spawn',
+        alias: 'Spown',
+        health: 23,
+        jump: 235,
+        secretIdentity: 'hello'
+    };
+})();
--- use-types-effectively-in-typescript/index.ts
+++ use-types-effectively-in-typescript/index.ts
@@ -1,2 +1,47 @@
-const unit: string = 'unit';
-const miles: 'MILES' = 'MILES';
+(function() {
+
+/**
+ * Interface VS Type alias
+ * 1. Type Alias union cannot be used for `implements`
+ * 2. Type Alias union cannot be used for `extends`
+ * 3. Declaration merging doesn't work with type alias
+ */
+
+
+interface AttackFunction {
+  (opponent: { alias: string; health: number }, attackWidth: number): number; // function
+}
+
+interface ComicBookCharacter {
+  secretIdentity: string;
+  readonly alias: string; // readonly
+  health: number;
+  name?: string;          // not required
+  attackFunction?: AttackFunction;
+}
+
+interface Spawn extends ComicBookCharacter {
+  jump: number;
+}
+
+const superHero: { secretIdentity: string; alias: string; health: number } = {
+  secretIdentity: '1',
+  alias: '1',
+  health: 1
+};
+const superVillain: ComicBookCharacter = {
+  secretIdentity: '1',
+  alias: '1',
+  health: 1,
+  name: 'hi'
+};
+
+const spawn: Spawn = {
+  name: 'Spawn',
+  alias: 'Spown',
+  health: 23,
+  jump: 235,
+  secretIdentity: 'hello'
+}
+
+})();
