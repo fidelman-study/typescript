2bef1046da2a5f3743ea2200c72c9eaed2390d58
Andrei Fidelman <andrei.fidelman@actum.cz>
2018-05-08 11:46:46 +0200

    Distinguishing between types of Strings in TypeScript

--- use-types-effectively-in-typescript/index.js
+++ use-types-effectively-in-typescript/index.js
@@ -1,10 +1,2 @@
-var thing = 3;
-function returnSomeThing(someThing) {
-    return someThing;
-}
-returnSomeThing(thing);
-var stuff = 3;
-function returnSomeStuff(someStuff) {
-    return someStuff;
-}
-console.log(returnSomeStuff(stuff));
+var unit = 'unit';
+var miles = 'MILES';
--- use-types-effectively-in-typescript/index.ts
+++ use-types-effectively-in-typescript/index.ts
@@ -1,19 +1,2 @@
-const thing: string | number | string[] | boolean = 3;
-
-function returnSomeThing(someThing: string | number | string[] | boolean):string | number | string[] | boolean {
-  return someThing;
-}
-
-returnSomeThing(thing);
-
-/* aliase */
-
-type stuffType = string | number | string[] | boolean | { name: string };
-
-const stuff = 3;
-
-function returnSomeStuff(someStuff: stuffType): stuffType {
-  return someStuff;
-}
-
-console.log(returnSomeStuff(stuff));
+const unit: string = 'unit';
+const miles: 'MILES' = 'MILES';
