bd0d4224e7c06f8622b436c092404761d22e0415
Andrei Fidelman <andrei.fidelman@actum.cz>
2018-05-08 13:54:47 +0200

    Sharing Class Behavior with Inheritance in TypeScript

--- use-types-effectively-in-typescript/index.js
+++ use-types-effectively-in-typescript/index.js
@@ -1,22 +1,5 @@
 (function () {
-    var User = /** @class */ (function () {
-        function User(_a) {
-            var name = _a.name, surname = _a.surname, phone = _a.phone;
-            this.name = name;
-            this.surname = surname;
-            this.phone = phone;
-        }
-        User.getName = function (name, surname) {
-            return name + " " + surname;
-        };
-        User.prototype.greeting = function () {
-            return "Hey " + User.getName(this.name, this.surname);
-        };
-        return User;
-    }());
-    var andrei = new User({
-        name: 'Andrei',
-        surname: 'Fidelman'
-    });
-    console.log('​andrei.greeting()', andrei.greeting());
+    // public (default)
+    // private – can be used only in Class
+    // protected – can be used in Extended Class as well
 })();
--- use-types-effectively-in-typescript/index.ts
+++ use-types-effectively-in-typescript/index.ts
@@ -1,36 +1,5 @@
 (function() {
-
-type UserConstructor = {
-  name: string;
-  surname: string;
-  phone?: string;
-};
-
-class User {
-  name: string;
-  surname: string;
-  phone?: string;
-
-  constructor({ name, surname, phone }: UserConstructor) {
-    this.name = name;
-    this.surname = surname;
-    this.phone = phone;
-  }
-
-  static getName(name: string, surname: string): string {
-    return `${name} ${surname}`;
-  }
-
-  greeting(): string {
-    return `Hey ${User.getName(this.name, this.surname)}`;
-  }
-}
-
-const andrei = new User({
-  name: 'Andrei',
-  surname: 'Fidelman'
-});
-
-console.log('​andrei.greeting()', andrei.greeting());
-
+  // public (default)
+  // private – can be used only in Class
+  // protected – can be used in Extended Class as well
 })();
