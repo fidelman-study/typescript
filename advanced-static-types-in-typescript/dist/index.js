9394610ce3eb30a74f8c7df2321aeb00be01568b
Andrei Fidelman <andrei.fidelman@actum.cz>
2018-05-19 18:47:57 +0200

    Query Properties with keyof and Lookup Types in TypeScript

--- advanced-static-types-in-typescript/dist/index.js
+++ advanced-static-types-in-typescript/dist/index.js
@@ -1,25 +1,9 @@
 "use strict";
-function tryParseInt(text) {
-    if (/^-?\d+$/.test(text)) {
-        return {
-            success: true,
-            value: parseInt(text, 10)
-        };
-    }
-    return {
-        success: false,
-        error: "Invalid number format"
-    };
-}
-function stringifyPaymentMethod(method) {
-    switch (method.kind) {
-        case 'cash':
-            return 'Cash';
-        case 'paypal':
-            return "PayPal (" + method.email + ")";
-        case 'creditcard':
-            return "Credit Card (*" + method.cardNumber.slice(-4) + ")";
-        default:
-            return 'Unknown method';
-    }
+var todo = {
+    id: 1,
+    text: 'Homework',
+    completed: false
+};
+function prop(obj, key) {
+    return obj[key];
 }
--- advanced-static-types-in-typescript/src/index.ts
+++ advanced-static-types-in-typescript/src/index.ts
@@ -1,49 +1,15 @@
-type Result =
-  | { success: true, value: number }
-  | { success: false, error: string };
-
-function tryParseInt(text: string): Result {
-  if (/^-?\d+$/.test(text)) {
-      return {
-          success: true,
-          value: parseInt(text, 10)
-      };
-  }
-  return {
-      success: false,
-      error: "Invalid number format"
-  };
+interface Todo {
+  id: number;
+  text: string;
+  completed: boolean;
 }
 
-// ========================================
+const todo: Todo = {
+  id: 1,
+  text:'Homework',
+  completed: false
+};
 
-
-interface Cash {
-  kind: 'cash';
-}
-
-interface PayPal {
-  kind: 'paypal';
-  email: string;
-}
-
-interface CreditCard {
-  kind: 'creditcard',
-  cardNumber: string;
-  securityCode: string;
-}
-
-type PaymentMethod = Cash | PayPal | CreditCard;
-
-function stringifyPaymentMethod(method: PaymentMethod): string {
-  switch (method.kind) {
-    case 'cash':
-      return 'Cash';
-    case 'paypal':
-      return `PayPal (${method.email})`;
-    case 'creditcard':
-      return `Credit Card (*${method.cardNumber.slice(-4)})`;
-    default:
-      return 'Unknown method';
-  }
+function prop<T, K extends keyof T>(obj: T, key: K): T[K] {
+  return obj[key];
 }
